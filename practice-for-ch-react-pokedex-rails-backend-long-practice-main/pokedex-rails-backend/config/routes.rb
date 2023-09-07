e8200d5aaf1e81048f10291bd52a4443c0d67d06
Vincent <vshan0816@gmail.com>
2023-09-07 17:29:44 -0400

    fix routes

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api, defaults: { format: :json } do
-    resources :pokemons, only: [:index, :show, :create, :update] do
+    get 'pokemon/types', to: 'pokemon#types'
+    resources :pokemon, only: [:index, :show, :create, :update] do
       resources :items, only: [:index, :create]
     end
-    resources :items, only: [:show, :destroy, :update]
+    resources :items, only: [:destroy, :update]
   end
 end
 
